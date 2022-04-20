0b3b827e6961e66c4403119e024d40d500caf149
Daniel Lizano <dlizano@outlook.com>
2022-04-20 12:26:31 -0600

    modificación en los nombres de archivos

---
+++ tareas/goldbach_serial/src/arrayGoldbach_uint64.h
@@ -0,0 +1,18 @@
+#ifndef ARRAYGOLDBACH_H
+#define ARRAYGOLDBACH_H
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stddef.h>
+
+typedef struct arrayGoldbach_uint64{
+    uint64_t contar;
+    uint64_t capacidad;
+    uint64_t* elemento;
+} arrayGoldbach64;
+
+
+
+
+#endif ARRAYGOLDBACH_H
