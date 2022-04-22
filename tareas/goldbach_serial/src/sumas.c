02c8c02250a3a1ebb9942c0272ea3a49ecda47a2
Daniel Lizano <dlizano@outlook.com>
2022-04-22 01:47:24 -0600

    Actualizamos métodos en sumas.c

--- tareas/goldbach_serial/src/sumas.c
+++ tareas/goldbach_serial/src/sumas.c
@@ -20,9 +20,24 @@ bool validar_par(char* entrada);
 
 bool validar_primo(char* entrada);
 
-bool es_fuerte(array_uint64_t suma, u_int64_t numero1, u_int64_t numero2);
+bool es_fuerte(array_uint64_t suma, uint64_t numero1, uint64_t numero2);
 
 bool es_debil(array_uint64_t suma, uint64_t numero1, uint64_t numero2,
-                uint64_t numero3);
+             uint64_t numero3);
 
-uint64_t get_valor(char* entrada);
+
+uint64_t get_valores(char* entrada);
+
+array_uint64_t generar_numeros_primos(uint64_t numero);
+
+array_uint64_t obtener_conjetura_fuerte(array_uint64_t numeros_primos,
+                                        uint64_t numero);
+
+array_uint64_t obtener_conjetura_debil(array_uint64_t numeros_primos,
+                                        uint64_t numero);
+                                    
+array_uint64_t obtener_sumas(bool es_par, uint64_t numero, 
+                            array_uint64_t numeros_primos);
+
+uint64_t obtener_conteo(bool es_par, array_uint64_t suma);
+
