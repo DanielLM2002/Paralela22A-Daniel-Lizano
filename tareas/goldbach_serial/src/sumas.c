4ae768062001bdb94a7fb1266328656d8b198d1d
Daniel Lizano <dlizano@outlook.com>
2022-04-22 00:53:55 -0600

    actualización de sumas.c

--- tareas/goldbach_serial/src/sumas.c
+++ tareas/goldbach_serial/src/sumas.c
@@ -1,2 +1,12 @@
 #include <sumas.h>
 
+typedef struct sumas{
+    bool es_valido;
+    bool es_negativo;
+    bool es_positivo;
+    bool es_par;
+    uint64_t valor;
+    uint64_t contar;
+    array_uint64_t numeros_primos;
+    array_uint64_t suma;
+} sumas_t;
