552fc4c469b51244ba53604c8372aa86d67afa0f
Daniel Lizano <dlizano@outlook.com>
2022-04-22 02:07:20 -0600

    desarrollo de sumas_create

--- tareas/goldbach_serial/src/sumas.c
+++ tareas/goldbach_serial/src/sumas.c
@@ -1,6 +1,7 @@
 #include <sumas.h>
 
 typedef struct sumas{
+    char* entrada;
     bool es_valido;
     bool es_negativo;
     bool es_positivo;
@@ -41,3 +42,21 @@ array_uint64_t obtener_sumas(bool es_par, uint64_t numero,
 
 uint64_t obtener_conteo(bool es_par, array_uint64_t suma);
 
+sumas_t* sumas_create(char* entrada){
+    sumas_t* sumas = (sumas_t*) calloc(1, sizeof(sumas_t));
+    sumas -> entrada = entrada;
+    sumas -> valor = 0;
+    sumas -> contar = 0;
+    sumas -> es_valido = validaciones(sumas -> entrada);
+    sumas -> es_positivo = false;
+    sumas -> es_negativo = false;
+    sumas -> es_par = false;
+    array_uint64_init(&sumas -> numeros_primos);
+    array_uint64_init(&sumas -> suma);
+    if (sumas -> es_valido){
+        sumas -> valor = get_valores(sumas -> entrada);
+        sumas -> es_par = validar_par(sumas -> entrada);
+        sumas -> es_negativo = validar_negativo(sumas -> entrada);
+    }
+    return sumas;
+}
