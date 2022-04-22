1b396e691abd12286a0dc9cdc421be0945664660
Daniel Lizano <dlizano@outlook.com>
2022-04-22 01:15:24 -0600

    actualización de sumas.c

--- tareas/goldbach_serial/src/sumas.c
+++ tareas/goldbach_serial/src/sumas.c
@@ -10,3 +10,19 @@ typedef struct sumas{
     array_uint64_t numeros_primos;
     array_uint64_t suma;
 } sumas_t;
+
+
+bool validaciones(char* entrada);
+
+bool validar_negativo(char* entrada);
+
+bool validar_par(char* entrada);
+
+bool validar_primo(char* entrada);
+
+bool es_fuerte(array_uint64_t suma, u_int64_t numero1, u_int64_t numero2);
+
+bool es_debil(array_uint64_t suma, uint64_t numero1, uint64_t numero2,
+                uint64_t numero3);
+
+uint64_t get_valor(char* entrada);
