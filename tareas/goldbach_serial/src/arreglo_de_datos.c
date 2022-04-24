98ad0c1cec3293d0fdde407500e838070f0ab3a1
Daniel Lizano <dlizano@outlook.com>
2022-04-24 05:22:51 -0600

    Update arreglo_de_datos.c

---
+++ tareas/goldbach_serial/src/arreglo_de_datos.c
@@ -0,0 +1,38 @@
+#include "arreglo_de_datos.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+typedef struct arreglo_nodo{
+
+    arreglo_nodo_t arreglo_sumas;
+
+    arreglo_nodo_t* next;
+
+    int64_t position;
+
+    int64_t value;
+
+    int validate;
+
+}arreglo_nodo_t;
+
+void arreglo_init(arreglo_de_datos_t* arreglo){
+    assert(arreglo);
+    arreglo -> cabeza = NULL;
+    arreglo -> cola = NULL;
+}
+
+int64_t get_value_arreglo(arreglo_nodo_t* arreglo){
+    return arreglo -> value;
+}
+
+int64_t get_posicion_arreglo(arreglo_nodo_t* nodo){
+    return nodo -> position;
+}
+
+arreglo_nodo_t* get_siguiente(arreglo_nodo_t* nodo){
+    return nodo -> next;
+}
+
+int64_t insert(arreglo_de_datos_t* arreglo, int64_t value);
