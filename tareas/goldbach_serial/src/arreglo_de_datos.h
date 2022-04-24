8c6b77c1c4ca3862392e3bac31cf9560271fd281
Daniel Lizano <dlizano@outlook.com>
2022-04-24 04:35:02 -0600

    actualización de clase array para soportar int64_t

---
+++ tareas/goldbach_serial/src/arreglo_de_datos.h
@@ -0,0 +1,37 @@
+#ifndef ARREGLO_DE_DATOS_H
+#define ARREGLO_DE_DATOS_H
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stddef.h>
+
+typedef struct arreglo_nodo arreglo_nodo_t;
+
+typedef struct arreglo_de_datos{
+    arreglo_nodo_t* cabeza;
+    arreglo_de_datos_t* cola;
+    uint64_t* elemento;
+} arreglo_de_datos_t;
+
+void arreglo_init(arreglo_de_datos_t* arreglo);
+
+int64_t get_value_arreglo(arreglo_nodo_t* arreglo);
+
+int64_t get_posicion_arreglo(arreglo_nodo_t* nodo);
+
+arreglo_nodo_t* get_siguiente(arreglo_nodo_t* nodo);
+
+int64_t insert(arreglo_de_datos_t* arreglo, int64_t value);
+
+arreglo_de_datos_t get_arreglo_sumas_goldbach(arreglo_de_datos_t* nodo);
+
+arreglo_de_datos_t search(arreglo_de_datos_t* arreglo, int64_t value);
+
+void nodo_destroy(arreglo_nodo_t* nodo);
+
+void arreglo_destroy(arreglo_de_datos_t* arreglo);
+
+int nodo_validate(arreglo_nodo_t* nodo);
+
+#endif
--- tareas/goldbach_serial/src/sumas.c
+++ tareas/goldbach_serial/src/sumas.c
@@ -1,7 +1,7 @@
 #include "sumas.h"
 typedef struct sumas{
     char* dato;
-    uint64_t valor;
+    uint64_t valores;
     uint64_t contar;
     array_uint64_t* orden;
     array_uint64_t* numeros_primos;
@@ -13,17 +13,25 @@ sumas_t* sumas_create(char* entrada){
     //iniciar campos de sumas
     sumas_t* sumas = (sumas_t*) calloc(1, sizeof(sumas_t));
     sumas -> dato = entrada;
-    sumas -> valor = 0;
+    sumas -> valores = 0;
     sumas -> contar = 0;
     array_uint64_init(&sumas -> numeros_primos);
     array_uint64_init(&sumas -> suma);
     return sumas; //retorno de la esctructura
+    
 }
 
-sumas_t* realizar_conjetura_fuerte(int64_t numero){
-    if(validar_par(numero)==true){
-        
-    }
+uint64_t valor = 0;
+uint64_t numero_entradas = 0;
+uint64_t tamanio = 1024;
+//uint64_t *entrada = (uint64_t *) malloc(tamanio * sizeof(entrada));
+
+
+void leer_archivo(){
+    while(fscanf(stdin, "%ld", &valor)==1){
+    entrada[numero_entradas] = valor;
+    numero_entradas++;
+}
 }
 
 
