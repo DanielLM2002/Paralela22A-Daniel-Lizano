518041bfc78ba0c911238c644254e80452aee899
Daniel Lizano <dlizano@outlook.com>
2022-04-26 16:51:05 -0600

    Update sumas.c

--- tareas/goldbach_serial/src/sumas.c
+++ tareas/goldbach_serial/src/sumas.c
@@ -9,17 +9,21 @@ int suma_goldbach_total(arreglo_de_datos_t* array){
     arreglo_de_datos_t* array_goldbach_actual = NULL;
     int error = EXIT_SUCCESS;
 
-while(current_position && error ==0){
-    array_goldbach_actual = get_arreglo_sumas_goldbach(current_position);
-    value = get_value_arreglo(current_position);
-    if(value < 0){
-        value = value + (value* -2);
-    }
+    while(current_position && error ==0){
+        array_goldbach_actual = get_arreglo_sumas_goldbach(current_position);
+        value = get_value_arreglo(current_position);
+        if(value < 0){
+            value = value + (value* -2);
+        }
 
-    else if(value > 5 && get_arreglo_sumas_goldbach(current_position) == 0){
-        error = suma_goldbach_numero(value, array_goldbach_actual);
+        else if(value > 5 && get_arreglo_sumas_goldbach(current_position) == 0){
+            error = suma_goldbach_numero(value, array_goldbach_actual);
+        }
+        current_position = get_siguiente(current_position);
     }
-    current_position = get_siguiente(current_position);
+    if(error){
+        fprintf(stderr,"The system lacks memory");
     }
+    return error;
 }
 
