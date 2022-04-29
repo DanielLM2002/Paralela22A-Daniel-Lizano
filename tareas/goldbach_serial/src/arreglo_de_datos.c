f0c05c5ba9ca628fca103912253ff93b2b73586c
DanielLM2002 <dlizano@outlook.com>
2022-04-29 21:48:15 +0000

    arreglo de codigo para brach principal

--- tareas/goldbach_serial/src/arreglo_de_datos.c
+++ tareas/goldbach_serial/src/arreglo_de_datos.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-typedef struct arreglo_nodo{
+typedef struct arreglo_nodo {
 
     
     arreglo_de_datos_t arreglo_sumas;
@@ -16,79 +16,80 @@ typedef struct arreglo_nodo{
 
     int validate;
 
-}arreglo_nodo_t;
+} arreglo_nodo_t;
 
-void arreglo_init(arreglo_de_datos_t* arreglo){
+void arreglo_init(arreglo_de_datos_t* arreglo) {
     assert(arreglo);
     arreglo -> cabeza = NULL;
     arreglo -> cola = NULL;
 }
 
-int64_t get_value_arreglo(arreglo_nodo_t* arreglo){
+int64_t get_value_arreglo(arreglo_nodo_t* arreglo) {
     return arreglo -> value;
 }
 
-int64_t get_posicion_arreglo(arreglo_nodo_t* nodo){
+int64_t get_posicion_arreglo(arreglo_nodo_t* nodo) {
     return nodo -> position;
 }
 
-arreglo_nodo_t* get_siguiente(arreglo_nodo_t* nodo){
+arreglo_nodo_t* get_siguiente(arreglo_nodo_t* nodo) {
     return nodo -> next;
 }
 
-int insert(arreglo_de_datos_t* arreglo, int64_t value, int valid){
+int insert(arreglo_de_datos_t* arreglo, int64_t value, int valid) {
     assert(arreglo);
     int error = EXIT_SUCCESS;
-    arreglo_nodo_t* new_nodo = (arreglo_nodo_t *) calloc(1, 
+    arreglo_nodo_t* new_nodo = (arreglo_nodo_t*) calloc(1, 
                                 sizeof(arreglo_nodo_t));
-    if(new_nodo){
+    if(new_nodo) {
         new_nodo -> value = value;
         new_nodo -> position = 1;
         new_nodo -> validate = valid;
         arreglo_init(&new_nodo -> arreglo_sumas);
 
-        if(arreglo -> cola == NULL){
+        if(arreglo -> cola == NULL) {
             arreglo -> cabeza = new_nodo;
             arreglo -> cola = new_nodo;
         }
-        else{
+        else {
             arreglo -> cola -> next = new_nodo;
             arreglo -> cola = arreglo -> cola -> next;
         }
-    }else{
+    } 
+    else {
         error = EXIT_FAILURE;
         fprintf(stderr,"Nos quedamos sin espacio de memoria");
     }
     return error;
 }
 
-arreglo_de_datos_t* get_arreglo_sumas_goldbach(arreglo_nodo_t* nodo){
+arreglo_de_datos_t* get_arreglo_sumas_goldbach(arreglo_nodo_t* nodo) {
     return &nodo->arreglo_sumas;
 }
 
-int nodo_validate(arreglo_nodo_t* nodo){
-    return nodo ->validate;
-}
 
-arreglo_nodo_t* search(arreglo_de_datos_t* arreglo, int64_t searching){
+arreglo_nodo_t* search(arreglo_de_datos_t* arreglo, int64_t searching) {
     assert(arreglo);
     arreglo_nodo_t* actual = arreglo ->cabeza;
-    while(actual->value && actual != searching){
-        actual = actual -> next;
+    while(actual->value) {
+        if(actual->value != searching){
+            actual = actual -> next;
+        }
     }
     return actual;
 }
-void nodo_destroy(arreglo_nodo_t* nodo){
+
+void nodo_destroy(arreglo_nodo_t* nodo) {
     nodo->value =0;
-    nodo_destroy(&nodo->arreglo_sumas);
+    arreglo_destroy(&nodo ->arreglo_sumas);
     free(nodo);
 }
 
-void arreglo_destroy(arreglo_de_datos_t* arreglo){
+void arreglo_destroy(arreglo_de_datos_t* arreglo) {
     assert(arreglo);
     arreglo_nodo_t* actual = arreglo->cabeza;
     arreglo_nodo_t* borrar = NULL;
-    while(actual){
+    while(actual) {
         borrar = actual -> next;
         nodo_destroy(actual);
         actual = borrar;
@@ -96,11 +97,13 @@ void arreglo_destroy(arreglo_de_datos_t* arreglo){
     arreglo->cabeza=NULL;
     arreglo->cola=NULL;
 }
+int nodo_validate(arreglo_nodo_t* nodo) {
+    return nodo->validate;
+}
 
-
-int array_insertion (arreglo_de_datos_t* arreglo, int64_t value, int valid){
+int array_insertion (arreglo_de_datos_t* arreglo, int64_t value, int valid) {
     assert(arreglo);
     int error = EXIT_SUCCESS;
-    error = cola_insertar(arreglo, value, valid);
+    error = insert(arreglo, value, valid);
     return error;
 }
--- tareas/goldbach_serial/src/entrada.c
+++ tareas/goldbach_serial/src/entrada.c
@@ -1,69 +1,76 @@
 #include "entrada.h"
 #include "sumas.h"
 
-void entrada_init(entrada_t* entry){
-    entrada_t* error = EXIT_SUCCESS;
+void entrada_init(entrada_t* entrada) {
+    entrada->error = EXIT_SUCCESS;
 }
 
-int64_t run(entrada_t* entry){
+int64_t run(entrada_t* entrada) {
     arreglo_de_datos_t array;
     arreglo_init(&array);
 
-    entrada_get_file(entry, &array);
+    entrada_get_file(entrada, &array);
     suma_goldbach_total(&array);
-    if(entry->error==0)
+    if(entrada->error==0)
         entrada_print(&array);
     else
         fprintf(stderr,"There was an error in the program");
     arreglo_destroy(&array);
-    return entry->error;
+    return entrada->error;
 }
 
-void entrada_get_file(entrada_t* entry, arreglo_de_datos_t* array){
+void entrada_get_file(entrada_t* entrada, arreglo_de_datos_t* array) {
     int64_t current_value = 0;
     int64_t valid = 0;
-    while(feof(stdin) == 0){
-        if(fscanf(stdin, "%ld", &current_value)==1){
+    while(feof(stdin)==0) {
+        if (fscanf(stdin, "%ld", &current_value) == 1) {
             entrada_validate(&valid);
-            entry->error=array_insertion(array,current_value,valid);
+            entrada -> error = array_insertion(array, current_value, valid);
             valid = 0;
+
         }
-        else{
-            char ignore = '\0';
-            while(ignore != EOF && ignore != '\0'){
-                ignore = fgetc(stdin);
-            }
+        else {
+            current_value = 0;
+            entrada ->error = insert(array, current_value,1);
         }
     }
 }
+void ignore_line() {
+    char ignore = '\0';
+    while (ignore != EOF && ignore != '\n') {
+        ignore = fgetc(stdin);
+    }
+}
 
-void entrada_validate(int64_t* is_valid){
-    if(errno == ERANGE){
+void entrada_validate(int64_t* is_valid) {
+    if (errno == ERANGE) {
         *is_valid = 1;
         clearerr(stdin);
         errno = 0;
     }
 }
 
-void entrada_print_sumas(arreglo_nodo_t* current, int64_t value){
+void entrada_print_sumas(arreglo_nodo_t* current, int64_t value) {
     int64_t position = 0;
     int64_t number = 0;
     arreglo_nodo_t* current_sum = get_arreglo_sumas_goldbach(current)->cabeza;
     bool impar = true;
-    if(value % 2 == 0){impar = false;}
+    if(value % 2 == 0) {
+        impar = false;
+    }
     int64_t counter = 0;
-    while(current_sum){
+    while (current_sum) {
         position = get_posicion_arreglo(current_sum);
         number = get_value_arreglo(current_sum);
-        if(position > 0){
+        if (position > 0) {
             fprintf(stdout, "%ld + ", number);
             number = 0;
             ++counter;
-            if(counter == 3 && impar == true){
+            if (counter == 3 && impar == true) {
                 fprintf(stdout,", ");
                 counter = 0;
             }
-            if(impar==false && counter == 2){
+            if (impar==false && counter == 2) {
                 fprintf(stdout, ", ");
                 counter = 0;
             }
@@ -75,23 +82,23 @@ void entrada_print_sumas(arreglo_nodo_t* current, int64_t value){
     }
 }
 
-void entrada_print(arreglo_de_datos_t* array){
+void entrada_print(arreglo_de_datos_t* array) {
     arreglo_nodo_t* actual = array->cabeza;
     int64_t value = 0;
     int64_t aux_value = 0;
 
-    while(actual){
+    while(actual) {
         value = get_value_arreglo(actual);
-        if(nodo_validate(actual)){
+        if(nodo_validate(actual)) {
             fprintf(stdout,"%ld: NA\n", value);
         }
-        if(value <= 5 && value >= 0){
+        if(value <= 5 && value >= 0) {
             fprintf(stdout,"%ld: NA\n", value);
         }
-        if(value > 5 || value < -5){
+        if(value > 5 || value < -5) {
             fprintf(stdout, "%ld: ", value);
             get_sumas_entrada(actual, aux_value);
-            if(value < -5){
+            if(value < -5) {
                 aux_value = value + (value * -2);
                 entrada_print_sumas(actual, aux_value);
             }
@@ -101,22 +108,24 @@ void entrada_print(arreglo_de_datos_t* array){
     }
 }
 
-void get_sumas_entrada(arreglo_nodo_t* actual, int64_t value){
+void get_sumas_entrada(arreglo_nodo_t* actual, int64_t value) {
     bool impar = true;
-    if(value % 2 == 0){impar = true;}
+    if(value % 2 == 0) {
+        impar = true;
+    }
     int counter = 0;
     int aux = 0;
     arreglo_nodo_t* current_sum = get_arreglo_sumas_goldbach(actual)->cabeza;
-    while(current_sum){
+    while(current_sum) {
         if(impar){
             ++counter;
         }
-        if(counter == 3){
+        if(counter == 3) {
             counter = 0;
             ++aux;
         }
         else{
-            if(counter == 2){
+            if(counter == 2) {
                 counter = 0;
                 ++aux;
             }
