eebc9271207e701ade91ada379a1e4f3253b3396
Daniel Lizano <63821814+DanielLM2002@users.noreply.github.com>
2022-05-27 23:56:35 -0600

    correcion de metodos para concurrencia

--- tareas/goldbach_pthread/src/sumas.c
+++ tareas/goldbach_pthread/src/sumas.c
@@ -57,7 +57,7 @@ int goldbach_par(int64_t numero, arreglo_t* cola_goldbach) {
     numero = numero + (numero * -2);
   }
   int64_t numero_aux = 0;
-  for (int i = 2; i < numero; i++) {//si no funca cambiar por int64
+  for (int64_t i = 2; i < numero; i++) {//si no funca cambiar por int64
     if (goldbach_es_primo(i) == true) {
       numero_aux = numero - i;
       if (i + numero_aux == numero &&
@@ -79,28 +79,30 @@ int goldbach_par(int64_t numero, arreglo_t* cola_goldbach) {
  */
 int goldbach_impar(int64_t numero, arreglo_t* cola_goldbach) {
   int error = EXIT_SUCCESS;
-
+  if (numero < -5) {
+    numero = numero + (numero * -2);
+  }
+  int64_t numero_temp = 0;
   for (int64_t i = 2; i < numero; i++) {
-    if (goldbach_es_primo(i) == true) {
+    if (goldbach_es_primo(i) == true && i != numero) {
       for (int64_t j = 2; j < numero; j++) {
-        if (goldbach_es_primo(j) == true) {
-          for (int64_t k = 2; k < numero; k++) {
-            if (i + j + k == numero && goldbach_es_primo(k)) {
-                if (i <= j && i <= k && j <= k) {
-                 error = arreglo_posible_insertar(cola_goldbach, i , 0);
-                 error = arreglo_posible_insertar(cola_goldbach, j, 0);
-                 error = arreglo_posible_insertar(cola_goldbach, k , 0);
-                } 
+        if (goldbach_es_primo(j) == true && j != numero) {
+          numero_temp = numero - (j + i);
+          if (i + j + numero_temp == numero && 
+           goldbach_es_primo(numero_temp)) {
+            if (i <= j && i <= numero_temp && j <= numero_temp) {
+              error = cola_posible_insertar(cola_goldbach, i, 0);
+              error = cola_posible_insertar(cola_goldbach, j, 0);
+              error = cola_posible_insertar(cola_goldbach, numero_temp, 0);
             }
           }
         }
       }
     }
   }
-
   return error;
-
 }
+
 /**
  * @brief enciar la suma del numero
  * 
@@ -109,11 +111,9 @@ int goldbach_impar(int64_t numero, arreglo_t* cola_goldbach) {
  * @return int 
  */
 int goldbach_suma_numero(int64_t numero, arreglo_t* cola_goldbach) {
-  
   if (numero % 2 == 0) {
     return goldbach_par(numero, cola_goldbach);
   } else {
     return goldbach_impar(numero, cola_goldbach);
   }
-
 }
