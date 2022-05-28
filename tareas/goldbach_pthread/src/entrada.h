67a528e37eb782224978cd1a0378bdb5f6acdce6
Daniel Lizano <63821814+DanielLM2002@users.noreply.github.com>
2022-05-28 00:44:08 -0600

    nuevo metodo para concurrencia
    nuevo metodo para concurrencia en entrada.h

--- tareas/goldbach_pthread/src/entrada.h
+++ tareas/goldbach_pthread/src/entrada.h
@@ -24,15 +24,18 @@ typedef struct interfaz {
 
 int entrada_ejecutar(int argc, char* argv[]);
 
-void entrada_get_file(arreglo_t * cola);
+int64_t entrada_validacion_entrada(int argc, char* argv[])
+
+void entrada_get_file(arreglo_t* cola);
 
 void ignorar_linea();
 
-void entrada_validate(int * is_invalid);
+void entrada_validate(int* is_invalid);
 
-void entrada_imprimir(arreglo_t * cola);
+void entrada_imprimir(arreglo_t* cola);
 
-void entrada_imprimir_goldbach(arreglo_nodo_t* actual, int64_t valor);
+void entrada_imprimir_goldbach(arreglo_nodo_t* actual, int64_t valor, 
+                                int64_t sumas);
 
 void entrada_conseguir_sumas(arreglo_nodo_t* actual, int64_t valor);
 
