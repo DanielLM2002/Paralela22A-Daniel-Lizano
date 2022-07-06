20eb839b88e92677868285c3cf83ea010fe2a77d
Daniel Lizano <dlizano@outlook.com>
2022-07-06 01:07:36 -0600

    se acarrea codigo de entrada

---
+++ tareas/goldbach_optimization/src/entrada.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include "arreglo_de_datos.h"
+#include "arreglo_de_sumas.h"
+#include "entrada.h"
+
+
+void get_file(FILE* entrada,  arreglo_dinamico_t* numeros) {
+    assert(entrada);
+    int64_t valor;
+    // Termina si encuentra un caracter o fin de linea
+    while (fscanf(entrada, "%ld", &valor) == 1) {
+        if (errno == ERANGE) {  // Cuando el num está fuera de rango.
+            arreglo_dinamico_append(numeros, valor, FUERA_DE_RANGO);
+        } else if (valor > 0) {  // Si el num es positivo.
+            arreglo_dinamico_append(numeros, valor, SIN_SUMAS);
+        } else {
+            arreglo_dinamico_append(numeros, valor, COMPLETO);
+        }
+        errno = 0;
+    }
+}
---
+++ tareas/goldbach_optimization/src/entrada.h
@@ -0,0 +1,9 @@
+#ifndef ENTRADA_H
+#define ENTRADA_H
+
+#include <stdio.h>
+#include "arreglo_de_datos.h"
+
+void get_file(FILE* entrada, arreglo_dinamico_t* numeros);
+
+#endif  // ENTRADA_H
