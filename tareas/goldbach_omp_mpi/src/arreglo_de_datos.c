47ebdb56063f34fc351215f85b7e5618342dab6a
Daniel Lizano <dlizano@outlook.com>
2022-07-23 01:52:07 -0600

    se importa codigo de tarea anterior

---
+++ tareas/goldbach_omp_mpi/src/arreglo_de_datos.c
@@ -0,0 +1,82 @@
+
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <omp.h>
+#include "arreglo_de_datos.h"
+#include "arreglo_de_sumas.h"
+
+
+
+
+
+
+/**
+ * @brief Inicializa el arreglo diamico con una capacity inicial de 2.
+ */
+
+void arreglo_init(numbers_t* num) {
+    assert(num);
+    num->capacity = 0;
+    num->size = 0;
+    num->nodes = NULL;
+}
+
+
+/**
+ * @brief Destruye los nodes creados y borra los valores anteriores.
+ */
+
+void arreglo_destroy(numbers_t* num) {
+    assert(num);
+    for (int64_t index = 0; index < (int64_t)num->size; index++) {
+        sums_destroy(num->nodes[index].sums, false);
+    }
+    num->capacity = 0;
+    num->size = 0;
+    free(num->nodes);
+}
+
+
+/**
+ * @brief Arega un elemento al arreglo y si no tiene espacioa para agregarar
+ * aumenta su capacida el doble de la capacida actual.
+ *
+ * @return Retorna los errores que se puedan dar al intentar
+ * aumentar la capacida del arreglo.
+ */
+
+int64_t arreglo_add_number(numbers_t* num, int64_t value, int state) {
+    assert(num);
+    int64_t error = EXIT_SUCCESS;
+    if (num->size == num->capacity) {
+        error = array_alloc_more(num);
+    }
+    // NOTE: memoria ya inicializada
+    num->nodes[num->size].number = value;
+    num->nodes[num->size].validation = state;
+    num->nodes[num->size].sums = sums_create();
+    num->size++;
+    return error;
+}
+
+
+/**
+ * @brief Intenta realocar más memoria para el arreglo.
+ *
+ * @return Códigos de error.
+ */
+
+int64_t array_alloc_more(numbers_t* num) {
+    size_t nueva_capacidad = 2 * (num->capacity? num->capacity : 1);
+    numberNode_t* nuevos_nodos = (numberNode_t*)realloc(num->nodes
+            , nueva_capacidad * sizeof(numberNode_t));
+    if (nuevos_nodos) {
+        num->capacity = nueva_capacidad;
+        num->nodes = nuevos_nodos;
+        return EXIT_SUCCESS;
+    } else {
+        // No se pudo aumentar el tamaño del arreglo
+        return EXIT_FAILURE;
+    }
+}
---
+++ tareas/goldbach_omp_mpi/src/arreglo_de_datos.h
@@ -0,0 +1,75 @@
+#ifndef ARREGLO_NUMEROS_H
+#define ARREGLO_NUMEROS_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <omp.h>
+
+#include "arreglo_de_sumas.h"
+
+enum {
+    COMPLETO,
+    FUERA_DE_RANGO,
+    SIN_SUMAS,
+    MENOR_A_5
+};
+
+/* -------------------------------*/
+/**
+ * @brief Guarda un number con su respectibo valor un conjunto de sums
+ * y una bandera para identificar su condición.
+ * @details Banderas:
+ * @details VALIDO: que el number se debe realizar todo el
+ * procedimiento normal.
+ * @details FUERA_DE_RANGO: que no se debe calcular y debe imprimir 'NA'.
+ * @details SIN_SUMAS: realiza el calculo pero solo imprime la cantidad
+ * de sums, sin mostrar cuales son.
+ */
+/* ---------------------------------*/
+typedef struct {
+    int64_t number;
+    sums_t* sums;
+    int validation;
+} numberNode_t;
+
+/* -------------------------------*/
+/**
+ * @brief Arreglo dinamico que guarda un conjunto de de nodosNumero.
+ */
+/* ---------------------------------*/
+typedef struct {
+    size_t capacity;
+    size_t size;
+    numberNode_t* nodes;
+} numbers_t;
+/* -------------------------------*/
+/**
+ * @brief Crea el arreglo dinámico para nodes number.
+ *
+ * @param arreglo dirección al registro donde opera.
+ */
+/* ---------------------------------*/
+void arreglo_init(numbers_t* num);
+/* -------------------------------*/
+/**
+ * @brief Destruye el arreglo dinámico que se solicite.
+ *
+ * @param arreglo Dirección al arreeglo que se desea eleiminar.
+ */
+/* ---------------------------------*/
+void arreglo_destroy(numbers_t* num);
+
+/* -------------------------------*/
+/**
+ * @brief Agrega un nodo number a un arreglo dinamico.
+ *
+ * @param _numeros Dirección al arreglo donde se busca opera
+ * @param _valor Es el number que se va a guardar en el nodo.
+ * @param _bandera Identificador del nodo.
+ *
+ * @return Retorna un código deerror si algo salió mal.
+ */
+/* ---------------------------------*/
+int64_t arreglo_add_number(numbers_t* num, int64_t value, int state);
+
+#endif  // ARREGLO_H
