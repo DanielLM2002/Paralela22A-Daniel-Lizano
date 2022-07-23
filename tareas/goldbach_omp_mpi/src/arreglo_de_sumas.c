e346c050509b8e36d89331f4edd02e62e6f17936
Daniel Lizano <dlizano@outlook.com>
2022-07-23 01:52:25 -0600

    ligeras modificaciones al arreglo

---
+++ tareas/goldbach_omp_mpi/src/arreglo_de_sumas.c
@@ -0,0 +1,83 @@
+
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#include "arreglo_de_sumas.h"
+
+/* -------------------------------*/
+/**
+ * @brief Inicializa el arreglo diamico con una capacity inicial de 2.
+ */
+/* ---------------------------------*/
+sums_t* sums_create(void) {
+    sums_t* sums = (sums_t*) calloc(1, sizeof(sums_t));
+    if (sums) {
+        sums->capacity = 0;
+        sums->size = 0;
+        sums->sumNode = NULL;
+    }
+    return sums;
+}
+
+/* -------------------------------*/
+/**
+ * @brief Destruye los elementos creados y borra los valores anteriores.
+ */
+/* ---------------------------------*/
+void sums_destroy(sums_t* sum_num, bool is_valid_num) {
+    assert(sum_num);
+    sum_num->capacity = 0;
+    sum_num->size = 0;
+    free(sum_num->sumNode);
+    if (!is_valid_num) {
+        free(sum_num);
+    }
+}
+
+/* -------------------------------*/
+/**
+ * @brief Arega un elemento al arreglo y si no tiene espacioa para agregarar
+ * aumenta su capacida el doble de la capacida actual.
+ *
+ * @return Retorna los errores que se puedan dar al intentar
+ * aumentar la capacida del arreglo.
+ */
+/* ---------------------------------*/
+int64_t sums_append(sums_t* sum_num, int64_t state1
+        , int64_t state2, int64_t state3) {
+    assert(sum_num);
+    int64_t error = EXIT_SUCCESS;
+    if (sum_num->size == sum_num->capacity) {
+        error = sums_more_space(sum_num);
+    }
+    // NOTE: memoria ya inicializada.
+    sum_num->sumNode[sum_num->size].state1 = state1;
+    sum_num->sumNode[sum_num->size].state2 = state2;
+    sum_num->sumNode[sum_num->size].state3 = state3;
+    sum_num->size++;
+    return error;
+}
+
+/* -------------------------------*/
+/**
+ * @brief Intenta realocar más memoria para el arreglo.
+ *
+ * @return Códigos de error.
+ */
+/* ---------------------------------*/
+int64_t sums_more_space(sums_t* sum_num) {
+    size_t new_space = 2 * (sum_num->capacity ? sum_num->capacity : 1);
+    // NOTE: creo Array nodoSuma 0x55555555b340
+    sumNode_t* nuevos_nodos = (sumNode_t*)realloc(sum_num->sumNode
+            , new_space * sizeof(sumNode_t));
+    if (nuevos_nodos) {
+        sum_num->capacity = new_space;
+        sum_num->sumNode = nuevos_nodos;
+        return EXIT_SUCCESS;
+    } else {
+        // No se pudo aumentar el tamaño del arreglo
+        return EXIT_FAILURE;
+    }
+}
---
+++ tareas/goldbach_omp_mpi/src/arreglo_de_sumas.h
@@ -0,0 +1,64 @@
+
+
+#ifndef ARREGLO_SUMAS_H
+#define ARREGLO_SUMAS_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/* -------------------------------*/
+/**
+ * @brief Guarda un suma que conciste en la suma de dos o tres numeros.
+ */
+/* ---------------------------------*/
+typedef struct {
+    int64_t state1;
+    int64_t state2;
+    int64_t state3;
+} sumNode_t;
+
+/* -------------------------------*/
+/**
+ * @brief Arreglo dinamico que guarda un conjunto de de nodosNumero.
+ */
+/* ---------------------------------*/
+typedef struct {
+    size_t capacity;
+    size_t size;
+    sumNode_t* sumNode;
+} sums_t;
+/* -------------------------------*/
+/**
+ * @brief Crea el arreglo dinámico para nodes suma.
+ *
+ * @param arreglo dirección al registro donde opera.
+ */
+/* ---------------------------------*/
+sums_t* sums_create(void);
+/* -------------------------------*/
+/**
+ * @brief Destruye el arreglo dinámico que se solicite.
+ *
+ * @param sum_num Dirección al arreeglo que se desea eleiminar.
+ * @param is_valid_num TRUE: es un arreglo de sums_t por lo que no se debe
+ * eliminar la dirección del arreglo que semando
+ */
+/* ---------------------------------*/
+void sums_destroy(sums_t* sum_num, bool is_valid_num);
+/* -------------------------------*/
+/**
+ * @brief Agrega un nodo suma a un arreglo dinamico.
+ *
+ * @param sum_num Dirección al arreglo donde se busca operar.
+ * @param _primero Valor del primer dato del objeto.
+ * @param _segundo Valor del state2 dato del objeto.
+ * @param _tercero Valor del state3 dato del objeto.
+ *
+ * @return Retorna un código deerror si algo salió mal.
+ */
+/* ---------------------------------*/
+int64_t sums_append(sums_t* sum_num, int64_t state1
+        , int64_t state2, int64_t state3);
+
+#endif  // ARREGLO_H
